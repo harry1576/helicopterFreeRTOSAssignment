001eb57f538042173a04c2283c6dbe524966e766
Jos Craw <jos@joscraw.net>
2020-07-15 18:02:35 +1200

    Added more doc as well as changes the compliler flags to re-enable optimisation

--- README.md
+++ README.md
@@ -13,11 +13,19 @@ user@linux~$ make
 user@linux~$ make flash
 ```
 
-## Debugging
+## Differences from stock repo
+This uses CMake so have some differences compared to the demo repo, this was made before that so for example this linker script is more complex. The CMakeLists.txt and files in [cmake](cmake) folder decribe the building of the project
 
+## Heli Lib
+To simplify the code as much as possible using a separate heli library found in [inc/Heli](inc/Heli)
+
+## Demo Code
+The original demo code from Andre's repo can be found in [demo-code/](demo-code)
+
+## Debugging
 TODO
 
 ## Authors
-Jos Craw
-Joshua Hulbert
-Harry Dobbs
+-   Jos Craw
+-   Joshua Hulbert
+-   Harry Dobbs
--- cmake/tiva-toolchain.cmake
+++ cmake/tiva-toolchain.cmake
@@ -13,8 +13,8 @@ set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}-objdump)
 set(CPU "-mcpu=cortex-m4")
 set(FPU "-mfpu=fpv4-sp-d16 -mfloat-abi=softfp")
 set(CMAKE_ASM_FLAGS " -mthumb ${CPU} ${FPU} -MD")
-set(CMAKE_C_FLAGS "-mthumb ${CPU} ${FPU} -std=gnu18 -O0 -ffunction-sections -fdata-sections -Wall -pedantic")
-set(CMAKE_CXX_FLAGS "-mthumb ${CPU} ${FPU} -Os -ffunction-sections -fdata-sections -MD -Wall -pedantic -fno-exceptions -fno-rtti")
+set(CMAKE_C_FLAGS "-mthumb ${CPU} ${FPU} -std=gnu18 -ffunction-sections -fdata-sections -Wall -pedantic")
+set(CMAKE_CXX_FLAGS "-mthumb ${CPU} ${FPU} -ffunction-sections -fdata-sections -MD -Wall -pedantic -fno-exceptions -fno-rtti")
 
 set(CMAKE_EXE_LINKER_FLAGS "--static -T${CMAKE_SOURCE_DIR}/inc/tiva_linkers/tm4c123gh6pm.lds -specs=${CMAKE_SOURCE_DIR}/inc/tiva_linkers/tiva.specs -specs=nosys.specs")
 
