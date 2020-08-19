fb21bed864698882459ec42d818e45b350185321
hdo27 <hdo27@uclive.ac.nz>
2020-08-19 14:53:48 +1200

    added update buttons

--- src/controller.c
+++ src/controller.c
@@ -205,7 +205,9 @@ void update_controllers(void)
 
             set_main_PWM(PWM_FREQUENCY, control_main);
             set_tail_PWM(PWM_FREQUENCY, control_tail);
-        
+
+            updateButtons();
+       
             if (checkButton(SWITCH) == RELEASED) {
                 set_helicopter_state(LANDING);
             }
@@ -213,7 +215,7 @@ void update_controllers(void)
 
         case LANDING:
 
-            if(abs(error_yaw) < 5 && percent_altitude > 10) // Get within 10 slots of start position and then begin decrementing height to 10
+            if(abs(error_yaw) < 5 && percent_altitude > 10) // Get within 5 slots of start position and then begin decrementing height to 10
             {
                 helicopter->target_altitude --;
             }
